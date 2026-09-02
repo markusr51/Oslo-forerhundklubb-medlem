47d1a313a7cffdfd8b381ad9e9a6176e9ef1b102
markusr51 <markusr51@icloud.com>
2026-09-02 16:57:45 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -172,7 +172,6 @@ async function renderPortalNavigation({
     ["expense-admin.html", "Utlegg", "expense-admin"],
     ["sms.html", "SMS", "sms"],
     ["email.html", "E-post", "email"],
-    ["email-reply-invitations.html", "E-post hurtigpåmelding", "email-reply"],
     ["communication-history.html", "Kommunikasjonshistorikk", "communication-history"]
   ];
 
